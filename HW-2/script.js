55779720916a5be9ba1d3f1f749b5d81908b4401
TheAthena02 <cl159183@umconnect.umt.edu>
2024-01-29 21:23:48 -0700

    HW2

---
+++ HW-2/script.js
@@ -0,0 +1,23 @@
+let log = console.log
+log("Favorite websites:");
+log(" ");
+log("pointerpointer.com");
+log("cat-bounce.com");
+log("zoomquilt.org");
+log(" ");
+log("Favorite Games:");
+log(" ");
+/*I am a captain on the Overwatch 2 team
+I am also one of the mods for the Esports Minecraft server
+we are hoping to host the server on the university server
+through an open port, we will see if and when this happens */
+log("Overwatch 2");
+log("Minecraft");
+log("Lethal Company");
+log(" ");
+log("Favorite Artists:");
+log(" ");
+//I feel very strongly about these artists, hence the long response
+log("Victoria Vincent (vewn - Youtube): An animator with a strong, agressive, childish style who creates massive animations. These animations tell stories of childhood, identity, gender, and inequality. These animations are well known for their use of cat imagery and relatable subject matter such as drugs, the internet, and work life to convey complex ideas. The simple cartoony style may turn away some, but I find this to help push the concepts presented in the animations.");
+log("Anna-Laura Sullivan (annalaura_art - instagram): Anna is a comic artist and animator who makes wholesome color pencil stories of small animals as people sharing small moments of intimacy and frienship, often offering inspirational advice or comments to the reader in doing so. Some comics involve simple happy moments. I find these comics to bring me to tears, as their beauty is so simple and yet so elegant.");
+log("April Grey (underscores - Spotify): is a Hyperpop/Pop-punk artist who creates dark, whimsical music. Her lyrics range from deeply introspecitve to the transgender experience, to completely non-sensical. I relate to a lot of what she sings about, and why shes a Hyperpop artist to begin with. The whole genre is about becoming something different from what you started as, especially in terms of gender. The pitched up vocals this genre is known for come from transgender artists trying to sound more feminine.");
